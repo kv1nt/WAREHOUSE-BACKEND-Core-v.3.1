466b45f151ed27f58aabc4796023ec09f5a711ab
Vitaliy <kv111nt@gmail.com>
2020-01-14 12:33:42 +0200

    Add login logic

---
+++ ASPNetApp2/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetApp2.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private LoginService _loginService;
+
+
+        public UserController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] Guid id)
+        {
+
+            try
+            {
+
+                if (_loginService.Login(id))
+                {
+                    return Ok("User logined successfully!");
+                }
+                else
+                {
+                    return NotFound("User not found! Please Register the user... ");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex);
+            }
+
+        }
+    }
+}
---
+++ Services/LoginService.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Services
+{
+    public class LoginService
+    {
+        private RepositoryContext _repositoryContext { get; set; }
+        public LoginService(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+        }
+
+       public bool Login(Guid id)
+       {
+            var user = _repositoryContext.Logins.Where(x => x.Id == id);
+            if(user != null) return true;
+              else return false;       
+        }
+
+        public bool Register(Login newUser)
+        {
+            var user = _repositoryContext.Logins.Where(x => x.Id == newUser.Id);
+            if (user == null)
+            {
+                _repositoryContext.Logins.Add(new Login
+                {
+                    Id = Guid.NewGuid(),
+                    Email = newUser.Email,
+                    Password = newUser.Password
+                }); return true;
+            } else return false;
+        }
+    }
+}
