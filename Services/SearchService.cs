3a7e87f2ad416e7359690b18794100331445dc62
Vitalii Palamarchuk <vitalii.palamarchuk@onseo.biz>
2020-08-20 14:18:51 +0300

    Add Search Service

--- Services/SearchService.cs
+++ Services/SearchService.cs
@@ -3,6 +3,7 @@ using Entities.Models;
 using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -17,25 +18,45 @@ namespace Services
             Type t = typeof(T);
             PropertyInfo[] propInfos1 = t.GetProperties();
             var props = propInfos1;
+            var propsNames = new Dictionary<string, string>();
+            var selectQuery = new StringBuilder($"SELECT * FROM {tableName} ");
+
+            var res = ps;
 
 
-             string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ASPNetDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //foreach (var prop in props)
+            //{
+            //    if (ps != null)
+            //    {
+            //        selectQuery.Append(prop.Name + $" = 'string' OR ");
+            //    }
+
+            //}
+            //var ee = selectQuery.ToString();
+
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ASPNetDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             using (var conn = new SqlConnection(connectionString))
                 try
                 {
                     conn.Open();
                     SqlCommand command = new SqlCommand();
-                    command.CommandText = "SELECT * FROM Product";
+                    command.CommandText = $"SELECT * FROM {tableName} ";
                     command.Connection = conn;
+                    SqlDataAdapter adapter = new SqlDataAdapter(selectQuery.ToString(), conn);
+                    DataSet ds = new DataSet();
+                    adapter.Fill(ds);
 
+          
                     SqlDataReader reader = command.ExecuteReader();
                     while (reader.Read())
                     {
-
-                        var x = reader[0];
-                        x = reader[1];
-                        x = reader[2];
+                        foreach (var item in props)
+                        {
+                            propsNames.Add(key: item.Name, value: reader[item.Name].ToString());
+                        }
+                      
+                      
                     }
 
                 }
@@ -52,7 +73,7 @@ namespace Services
         public static void GetFromParams()
         {
             var search = new SearchService();
-            search.Search(new Product { Name = "Sosige", Price = 100 }, "Products");
+            search.Search(new Product { Name = "Sosige", Price = 100 }, "Product");
         }
     }
 }
